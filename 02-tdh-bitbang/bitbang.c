0f82c819d232b1ea20d802eb1971010af3710441
Troy D. Hanson <tdh@tkhanson.net>
2016-01-21 22:43:04 -0500

    edit

--- 02-tdh-bitbang/bitbang.c
+++ 02-tdh-bitbang/bitbang.c
@@ -1,4 +1,4 @@
-                                                       /* Blinker Demo */
+                                                       /* Bitbang Demo */
 
 // ------- Preamble -------- //
 #include <avr/io.h>                        /* Defines pins, ports, etc */
