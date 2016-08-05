f5ba08d7df6c84e4fb059d88db6364cb39460c2f
Troy D. Hanson <tdh@tkhanson.net>
2016-08-05 18:41:37 +0000

    wip

--- 06-tdh-oscilloscope/ascope.c
+++ 06-tdh-oscilloscope/ascope.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "pinDefines.h"
+#include "limits.h"
 #include "USART.h"
 
 #define MAX_SIGNAL_HZ 120
@@ -33,6 +34,29 @@ static inline void initFreerunningADC(void) {
 char samples[NSAMPLES];
 unsigned n=0;
 int above_midpoint; /* whether last sample was above (max-min)/2 */
+char min, max;
+
+void find_extremes() {
+  min = SCHAR_MAX; /* set to opposite extreme initially */
+  max = SCHAR_MIN; /* same */
+  unsigned i;
+  for(i=0; i < NSAMPLES; i++) {
+    if (samples[i] > max) max = samples[i];
+    if (samples[i] < min) min = samples[i];
+  }
+}
+
+int cross_midpoint(char sample) {
+  char mid = (max - min) / 2;
+  if ((sample > mid) && above_midpoint) return 0;
+  if ((sample <= mid) && !above_midpoint) return 0;
+  above_midpoint = (sample > mid) ? 1 : 0;
+  return 1;
+}
+
+void toggle_speaker() {
+}
+
 
 int main(void) {
   initUSART();
@@ -46,6 +70,8 @@ int main(void) {
     /* find min,max in nsamples. we don't worry about the fact that the 
      * buffer is initially empty. it fills up in seconds. 
      */
+     find_extremes();
+     if (cross_midpoint(sample)) toggle_speaker(); /* match hz of signal */
     _delay_ms(SAMPLE_INTERVAL);
   }                                                  /* End event loop */
   return (0);                            /* This line is never reached */
